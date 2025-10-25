a35231b0f47b06a5a3fbc50a6eea95a932e0aa31
Foxymansama <dimalatoho1448@gmail.com>
2025-10-25 17:16:39 +0300

    update first program

--- first_program.asm
+++ first_program.asm
@@ -3,32 +3,28 @@
 
 #
 # Секція .data (змінні)
-# Тут ми заздалегідь готуємо рядки, які будемо "відображати"
 #
 .data
-    # Рядок для кроку 2.2.2
-    msg_0000: .asciz "Індикатор: 0000\n"
-    len_0000: .quad $ - msg_0000
+    # Це наш шаблон. Ми будемо змінювати 'XXXX' "на льоту".
+    # Українські літери займають 2 байти кожна в UTF-8.
+    # "Індикатор: " = 18 байт + 2 байти (двокрапка + пробіл) = 20 байт.
+    # Отже, 'X' починаються з 20-го байта.
+    msg_template: db 'Індикатор: XXXX', 0x0A
+    len_template: equ $ - msg_template
+    
+    # Визначимо адреси 'X' для зручності
+    PTR_CHAR_1: equ msg_template + 20  # Перший 'X'
+    PTR_CHAR_2: equ msg_template + 21  # Другий 'X'
+    PTR_CHAR_3: equ msg_template + 22  # Третій 'X'
+    PTR_CHAR_4: equ msg_template + 23  # Четвертий 'X'
 
-    # Рядок для кроку 2.2.4
-    msg_0F00: .asciz "Індикатор: 0F00\n"
-    len_0F00: .quad $ - msg_0F00
+    # "Комірка пам'яті" для кроку 2.2.9
+    memory_cell_0001h: .byte 0xAB
 
-    # Рядок для кроку 2.2.7
-    msg_F000: .asciz "Індикатор: F000\n"
-    len_F000: .quad $ - msg_F000
-
-    # Рядок для кроку 2.2.9
-    # Ми симулюємо "комірку 0001h", яка містить 0xAB
-    memory_cell_0001h: .byte 0xAB 
-    # Рядок, який показує F0 (з акумулятора) та AB (з пам'яті)
-    msg_F0AB: .asciz "Індикатор: F0AB\n"
-    len_F0AB: .quad $ - msg_F0AB
-
-    # Змінна для затримки (1 секунда)
+    # Змінна для затримки
     delay_time:
-        .quad 1     # tv_sec (секунди)
-        .quad 0     # tv_nsec (наносекунди)
+        .quad 1      # tv_sec (секунди)
+        .quad 0      # tv_nsec (наносекунди)
 
 #
 # Секція .text (код)
@@ -36,59 +32,130 @@
 .text
 _start:
     # 2.2.1 Обнулити акумулятор.
-    xor rax, rax            # (Використовуємо 64-бітний RAX)
+    xor rax, rax            # (al = 0x00)
 
-    # 2.2.2 Обнулити індикатор HG1. (Друкуємо "0000")
-    mov rax, 1              # (Системний виклик sys_write = 1)
-    mov rdi, 1              # (Файловий дескриптор stdout = 1)
-    lea rsi, [msg_0000]
-    mov rdx, [len_0000]
-    syscall
+    # 2.2.2 Обнулити індикатор HG1.
+    # Ми записуємо символи '0' (ASCII 0x30) у наш шаблон
+    mov byte [PTR_CHAR_1], '0'
+    mov byte [PTR_CHAR_2], '0'
+    mov byte [PTR_CHAR_3], '0'
+    mov byte [PTR_CHAR_4], '0'
+    call print_message      # Друкуємо "Індикатор: 0000\n"
 
     # 2.2.3 Завантажити акумулятор числом 0Fh.
-    mov al, 0x0F            # (Використовуємо 8-бітний AL)
+    mov al, 0x0F
 
-    # 2.2.4 Відобразити на HG1 «OF00». (Друкуємо "0F00")
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [msg_0F00]
-    mov rdx, [len_0F00]
-    syscall
+    # 2.2.4 Відобразити на HG1 «OF00».
+    # al = 0x0F. rsi = адреса першого 'X'
+    lea rsi, [PTR_CHAR_1]
+    call byte_to_hex_string # Конвертуємо 0x0F -> '0' і 'F'
+                            # і пишемо їх у [PTR_CHAR_1] та [PTR_CHAR_2]
+    # Залишаємо '00' наприкінці
+    mov byte [PTR_CHAR_3], '0'
+    mov byte [PTR_CHAR_4], '0'
+    call print_message      # Друкуємо "Індикатор: 0F00\n"
 
     # 2.2.5 Сформувати затримку.
-    mov rax, 35             # (Системний виклик nanosleep = 35)
-    lea rdi, [delay_time]
-    mov rsi, 0              # (Другий аргумент = NULL)
-    syscall
+    call do_delay
 
     # 2.2.6 Перетворити вміст акумулятора з 0Fh на F0h.
-    # (AL все ще = 0x0F з кроку 2.2.3)
     mov cl, 4
-    rol al, cl              # (AL тепер 0xF0)
+    rol al, cl              # (al тепер 0xF0)
 
-    # 2.2.7 Відобразити ... «F000». (Друкуємо "F000")
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [msg_F000]
-    mov rdx, [len_F000]
-    syscall
+    # 2.2.7 Відобразити ... «F000».
+    # al = 0xF0. rsi = адреса першого 'X'
+    lea rsi, [PTR_CHAR_1]
+    call byte_to_hex_string # Конвертуємо 0xF0 -> 'F' і '0'
+    # Залишаємо '00' наприкінці
+    mov byte [PTR_CHAR_3], '0'
+    mov byte [PTR_CHAR_4], '0'
+    call print_message      # Друкуємо "Індикатор: F000\n"
 
     # 2.2.8 Сформувати затримку.
-    mov rax, 35
-    lea rdi, [delay_time]
-    mov rsi, 0
-    syscall
+    call do_delay
 
     # 2.2.9 Зчитати ... 0001h і відобразити ... F0xx
-    # (Ми "зчитали" 0xAB і надрукуємо "F0AB")
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [msg_F0AB]
-    mov rdx, [len_F0AB]
-    syscall
+    # al все ще 0xF0. Конвертуємо його
+    lea rsi, [PTR_CHAR_1]
+    call byte_to_hex_string # Пишемо 'F' і '0' у перші два 'X'
+    
+    # Тепер зчитуємо 0xAB з пам'яті
+    mov al, [memory_cell_0001h] # al = 0xAB
+    # і конвертуємо його у другі два 'X'
+    lea rsi, [PTR_CHAR_3]
+    call byte_to_hex_string # Пишемо 'A' і 'B' у [PTR_CHAR_3] та [PTR_CHAR_4]
+    
+    call print_message      # Друкуємо "Індикатор: F0AB\n"
 
     # --- Завершення програми ---
-    mov rax, 60             # (Системний виклик sys_exit = 60)
-    mov rdi, 0              # (Код виходу 0 = успіх)
+    mov rax, 60
+    mov rdi, 0
     syscall
 
+# -------------------------------------------------
+# ДОПОМІЖНІ ФУНКЦІЇ (ПРОЦЕДУРИ)
+# -------------------------------------------------
+
+#
+# print_message: Друкує наш msg_template на екран
+#
+print_message:
+    mov rax, 1              # sys_write
+    mov rdi, 1              # stdout
+    lea rsi, [msg_template] # Наш шаблон
+    mov rdx, len_template   # Його довжина
+    syscall
+    ret
+
+#
+# do_delay: Викликає затримку в 1 секунду
+#
+do_delay:
+    mov rax, 35             # nanosleep
+    lea rdi, [delay_time]   # Адреса структури часу
+    mov rsi, 0              # NULL
+    syscall
+    ret
+
+#
+# byte_to_hex_string: Конвертує 1 байт (з AL) у 2 ASCII-символи
+#                     і записує їх за адресою в RSI.
+#                     Наприклад: al=0xF0, rsi=адреса -> запише 'F' і '0'
+#
+byte_to_hex_string:
+    push rax                # Зберігаємо оригінальний rax (з 0xF0)
+    
+    # 1. Конвертуємо старший нібл (перші 4 біти)
+    shr al, 4               # al = 0xF0 -> al = 0x0F
+    call nybble_to_ascii    # Конвертуємо 0x0F -> 'F'
+    mov [rsi], al           # Записуємо 'F' за адресою в rsi
+
+    pop rax                 # Відновлюємо rax (знову 0xF0)
+
+    # 2. Конвертуємо молодший нібл (останні 4 біти)
+    and al, 0x0F            # al = 0xF0 -> al = 0x00
+    call nybble_to_ascii    # Конвертуємо 0x00 -> '0'
+    mov [rsi+1], al         # Записуємо '0' у наступний байт
+
+    ret                     # Повертаємось
+
+#
+# nybble_to_ascii: Конвертує 4-бітне число (0-15) в AL
+#                  у ASCII-символ ('0'-'9' або 'A'-'F')
+#
+nybble_to_ascii:
+    cmp al, 10              # Порівнюємо з 10
+    jl .is_digit            # Якщо менше (<), це цифра ('0'-'9')
+
+    # .is_letter (Це 'A'-'F')
+    add al, 0x37            # Додаємо 0x37 ('A' - 10)
+                            # Наприклад: 10 (0xA) + 0x37 = 0x41 ('A')
+                            #            15 (0xF) + 0x37 = 0x46 ('F')
+    ret
+
+.is_digit:
+    # Це '0'-'9'
+    add al, 0x30            # Додаємо 0x30 ('0')
+                            # Наприклад: 0 + 0x30 = 0x30 ('0')
+                            #            9 + 0x30 = 0x39 ('9')
+    ret
